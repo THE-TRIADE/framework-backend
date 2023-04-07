9063698673c8dc4992c4300ee3fce435f69c8e47
Lucas Nogueira <lucas.nogueira.072@ufrn.edu.br>
2023-04-06 23:11:46 -0300

    Comment user stuff

--- src/main/java/imd/ufrn/familyroutine/repository/UserRepositoryImpl.java
+++ src/main/java/imd/ufrn/familyroutine/repository/UserRepositoryImpl.java
@@ -1,50 +1,50 @@
-package imd.ufrn.familyroutine.repository;
+// package imd.ufrn.familyroutine.repository;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.jdbc.core.JdbcTemplate;
+// import org.springframework.stereotype.Repository;
 
-import imd.ufrn.familyroutine.model.User;
+// import imd.ufrn.familyroutine.model.User;
 
-@Repository
-public class UserRepositoryImpl implements UserRepository {
+// @Repository
+// public class UserRepositoryImpl implements UserRepository {
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+//     @Autowired
+//     private JdbcTemplate jdbcTemplate;
 
 
-    @Override
-    public List<User> findAll() {
-        String sql = "SELECT * FROM `user`";
+//     @Override
+//     public List<User> findAll() {
+//         String sql = "SELECT * FROM `user`";
 
-        return jdbcTemplate.query(sql, 
-        (resultSet, rowNumber) -> new User(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getString("email")));
-    }
+//         return jdbcTemplate.query(sql, 
+//         (resultSet, rowNumber) -> new User(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getString("email")));
+//     }
 
-    @Override
-    public User finById(Integer id) {
-        String sql = "SELECT * FROM `user` WHERE id = " + id;
+//     @Override
+//     public User finById(Integer id) {
+//         String sql = "SELECT * FROM `user` WHERE id = " + id;
 
-        return jdbcTemplate.query(sql, 
-        (resultSet, rowNumber) -> new User(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getString("email"))).get(0);
-    }
+//         return jdbcTemplate.query(sql, 
+//         (resultSet, rowNumber) -> new User(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getString("email"))).get(0);
+//     }
 
-    @Override
-    public User save(User user) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
-    }
+//     @Override
+//     public User save(User user) {
+//         // TODO Auto-generated method stub
+//         throw new UnsupportedOperationException("Unimplemented method 'save'");
+//     }
 
-    @Override
-    public void deleteAll() {
-        jdbcTemplate.execute("DELETE * FROM user");
-    }
+//     @Override
+//     public void deleteAll() {
+//         jdbcTemplate.execute("DELETE * FROM user");
+//     }
 
-    @Override
-    public void deleteById(Integer id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
-    }
-}
+//     @Override
+//     public void deleteById(Integer id) {
+//         // TODO Auto-generated method stub
+//         throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+//     }
+// }
--- src/main/java/imd/ufrn/familyroutine/service/UserService.java
+++ src/main/java/imd/ufrn/familyroutine/service/UserService.java
@@ -1,23 +1,24 @@
-package imd.ufrn.familyroutine.service;
+// package imd.ufrn.familyroutine.service;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
 
-import imd.ufrn.familyroutine.model.User;
-import imd.ufrn.familyroutine.repository.UserRepository;
+// import imd.ufrn.familyroutine.model.Person;
+// import imd.ufrn.familyroutine.repository.UserRepository;
 
-@Service
-public class UserService {
-    @Autowired
-    private UserRepository userRepository;
+// @Service
+// public class UserService {
+//     // @Autowired
+//     // private UserRepository userRepository;
 
-    public List<User> findAll() {
-        return this.userRepository.findAll();
-    }
+//     public List<Person> findAll() {
+//         return this.userRepository.findAll();
+//     }
 
-    public User findUserById(Integer id) {
-        return this.userRepository.finById(id);
-    }
-}
+//     public Person findUserById(Integer id) {
+//         return this.userRepository.findById(id);
+//     }
+
+// }
