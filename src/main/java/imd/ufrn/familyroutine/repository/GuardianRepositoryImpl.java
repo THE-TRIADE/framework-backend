3b95e8082db43349f537c2d0c180a120629e8b5b
Lucas Nogueira <lucas.nogueira.072@ufrn.edu.br>
2023-04-07 23:52:36 -0300

    Finaliza ajustes no repository de Guardian
    Co-authored-by: Leonardo David <leodavidbr@users.noreply.github.com>
    Co-authored-by: anamgholandadev <anamgholandadev@users.noreply.github.com>

--- src/main/java/imd/ufrn/familyroutine/repository/GuardianRepositoryImpl.java
+++ src/main/java/imd/ufrn/familyroutine/repository/GuardianRepositoryImpl.java
@@ -1,12 +1,18 @@
 package imd.ufrn.familyroutine.repository;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import imd.ufrn.familyroutine.model.Guardian;
+import imd.ufrn.familyroutine.repository.mappers.GuardianMapper;
 
 @Repository
 public class GuardianRepositoryImpl implements GuardianRepository {
@@ -22,28 +28,38 @@ public class GuardianRepositoryImpl implements GuardianRepository {
 
     @Override
     public Guardian findById(Long id) {
-        String sql = "SELECT * FROM guardian WHERE id = " + id;
+        String sql = "SELECT * FROM guardian WHERE id = ?";
         return jdbcTemplate.queryForObject(sql, new Object[] { id }, new GuardianMapper());
     }
 
     @Override
     public Guardian save(Guardian guardian) {
-        String sql = "INSERT INTO guardian (id, `name`, cpf, birthDate, email, `password`) VALUES (?,?,?,?,?,?)";
-        jdbcTemplate.update(sql, guardian.getId(), guardian.getName(),  guardian.getCpf(), 
-                    guardian.getBirthDate(), guardian.getEmail(),  guardian.getPassword());
-        return null;
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        String sql = "INSERT INTO guardian (`name`, cpf, birthDate, email, `password`) VALUES (?,?,?,?,?)";
+
+        jdbcTemplate.update(connection -> { 
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, guardian.getName());
+            ps.setString(2, guardian.getCpf());
+            ps.setDate(3, (Date) guardian.getBirthDate());
+            ps.setString(4, guardian.getEmail());
+            ps.setString(5, guardian.getPassword());
+            return ps;
+        }, keyHolder);
+    
+        return this.findById(keyHolder.getKey().longValue());
     }
 
     @Override
     public void deleteById(Long id) {
-        String sql = "DELETE * FROM guardian WHERE id = " + id;
-        jdbcTemplate.update(sql, id > 0);
+        String sql = "DELETE FROM guardian WHERE id = ?";
+        jdbcTemplate.update(sql, new Object[] { id });
     }
 
     @Override
     public void deleteAll() {
-        String sql = "DELETE * FROM guardian";
-        jdbcTemplate.update(sql, true);
+        String sql = "DELETE FROM guardian";
+        jdbcTemplate.update(sql);
     }
      
 
