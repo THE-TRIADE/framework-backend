668fde4103b1ca76ac975ec81a8e0b35e3dc00ec
leodavidbr <rbdivadoel@gmail.com>
2023-04-08 23:30:39 -0300

    CrossOrigin annotation on controllers

--- src/main/java/imd/ufrn/familyroutine/controller/DependentController.java
+++ src/main/java/imd/ufrn/familyroutine/controller/DependentController.java
@@ -3,6 +3,7 @@ package imd.ufrn.familyroutine.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import imd.ufrn.familyroutine.model.Dependent;
 import imd.ufrn.familyroutine.service.DependentService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/dependent")
 public class DependentController {
--- src/main/java/imd/ufrn/familyroutine/controller/GuardianController.java
+++ src/main/java/imd/ufrn/familyroutine/controller/GuardianController.java
@@ -3,6 +3,7 @@ package imd.ufrn.familyroutine.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import imd.ufrn.familyroutine.model.Guardian;
 import imd.ufrn.familyroutine.service.GuardianService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/guardian")
 public class GuardianController {
