b05bcb87c00ead33106f09b660d4e02c371c15cd
Lucas Nogueira <lucas.nogueira.072@ufrn.edu.br>
2023-04-09 21:48:35 -0300

    Cria exceção raiz do projeto

---
+++ src/main/java/imd/ufrn/familyroutine/FamilyRoutineException.java
@@ -0,0 +1,10 @@
+package imd.ufrn.familyroutine;
+
+public class FamilyRoutineException extends RuntimeException {
+    public FamilyRoutineException () {}
+
+    public FamilyRoutineException (Throwable cause) {
+        super(cause);
+    }
+
+}
