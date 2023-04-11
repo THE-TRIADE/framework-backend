2abef83a6272fdfb898202ad50cf567241d19614
Lucas Nogueira <lucas.nogueira.072@ufrn.edu.br>
2023-04-10 22:00:24 -0300

    Cria classe RestResponse para encapsular respostas com mensagens de falha

--- src/main/java/imd/ufrn/familyroutine/controller/GlobalHandlerException.java
+++ src/main/java/imd/ufrn/familyroutine/controller/GlobalHandlerException.java
@@ -1,16 +1,15 @@
 package imd.ufrn.familyroutine.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import imd.ufrn.familyroutine.model.RestResponse;
 import imd.ufrn.familyroutine.service.exception.EntityNotFoundException;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
@@ -18,10 +17,13 @@ import imd.ufrn.familyroutine.service.exception.EntityNotFoundException;
 public class GlobalHandlerException extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({ EntityNotFoundException.class })
-    public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException ex) {
-        Map<String, Object> responseBody = new HashMap<>();
-        responseBody.put("message", ex.getMessage());
-        return new ResponseEntity<>(responseBody, HttpStatus.NOT_FOUND);
+    public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException ex, WebRequest request) {
+        RestResponse response = RestResponse.builder()
+            .status(HttpStatus.NOT_FOUND)
+            .message(ex.getMessage())
+            .path(request.getDescription(false).substring(4))
+            .build();
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
 }
