fcfdbac686fa8638f949833ce323ed76ffdf1baf
leodavidbr <rbdivadoel@gmail.com>
2023-05-18 19:40:06 -0300

    Created hashCode and equals of FamilyGroupResponse

--- src/main/java/imd/ufrn/familyroutine/model/api/response/FamilyGroupResponse.java
+++ src/main/java/imd/ufrn/familyroutine/model/api/response/FamilyGroupResponse.java
@@ -26,4 +26,29 @@ public class FamilyGroupResponse {
     public void setDependents(List<Dependent> dependents) {
         this.dependents = dependents;
     }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        FamilyGroupResponse other = (FamilyGroupResponse) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+
 }
