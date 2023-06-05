b3d76380e3c6e3f5903375332a4107557834b5b3
Ana Maria <ana.mgholandadev@gmail.com>
2023-06-05 20:23:15 -0300

    Casos de teste para atualizar e excluir guarda

--- src/test/java/imd/ufrn/familyroutine/service/GuardServiceTest.java
+++ src/test/java/imd/ufrn/familyroutine/service/GuardServiceTest.java
@@ -4,30 +4,97 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import imd.ufrn.familyroutine.model.Guard;
+import imd.ufrn.familyroutine.model.GuardianRole;
+import imd.ufrn.familyroutine.model.api.GuardMapper;
+import imd.ufrn.familyroutine.model.api.request.GuardRequest;
+import imd.ufrn.familyroutine.repository.GuardRepository;
+
 // TODO Ana Maria
 @ExtendWith(SpringExtension.class)
 public class GuardServiceTest {
+    @InjectMocks
+    private GuardService guardService;
+
+    @Mock
+    private GuardRepository guardRepository;
+
+    @Mock
+    private GuardMapper guardMapper;
+
+    @Mock
+    private ValidationService validationService;
 
     @Nested
     public class UpdateGuard {
-        /*
-         * Positivo simples - ID que existe e é um Long
-         * Negativo com entradas válidas  - ID não existe, mas é um Long
-         */
+        @Test
+        public void shouldUpdateGuardAndReturnUpdatedGuard() {
+            Long guardId = 1L;
+
+            Guard guard = new Guard();
+            guard.setId(guardId);
+            guard.setDependentId(1L);
+            guard.setGuardianId(1L);
+            guard.setGuardianRole(GuardianRole.FATHER);
+
+            GuardRequest guardRequest = new GuardRequest();
+            guardRequest.setGuardianRole(GuardianRole.MOTHER);
+            guardRequest.setDependentId(1L);
+            guardRequest.setGuardianId(1L);
+
+            Guard guardUpdated = new Guard();
+            guardUpdated.setId(guardId);
+            guardUpdated.setDependentId(guardRequest.getDependentId());
+            guardUpdated.setGuardianId(guardRequest.getGuardianId());
+            guardUpdated.setGuardianRole(guardRequest.getGuardianRole());
+
+            Mockito.when(guardMapper.mapGuardRequestToGuard(guardRequest)).thenReturn(guardUpdated);
+
+            Mockito.when(guardRepository.findById(guardId)).thenReturn(Optional.of(guard));
+
+            Mockito.when(guardRepository.update(guard)).thenReturn(guard);
+
+            Mockito.when(validationService.validDaysOfWeekOrError(guardRequest.getDaysOfWeek()))
+                    .thenReturn(guardRequest.getDaysOfWeek());
+
+            Guard updatedGuard = guardService.updateGuard(guardId, guardRequest);
+
+            assertEquals(guard, updatedGuard);
+        }
     }
 
     @Nested
     public class DeleteGuardById {
-        /*
-         * Positivo simples - ID que existe e é um Long
-         * Negativo com entradas válidas  - ID não existe, mas é um Long
-         */
-    }
+        @Test
+        public void shouldDeleteGuardById() {
+            Long guardId = 1L;
 
-    @Nested
-    public class DeleteAllGuards {
-        /*
-         * Positivo simples
-         */
+            guardService.deleteGuardById(guardId);
+
+            verify(guardRepository, times(1)).deleteById(guardId);
+        }
     }
 }
+
+// @Nested
+// public class DeleteAllGuards {
+// /*
+// * Positivo simples
+// */
+// }
